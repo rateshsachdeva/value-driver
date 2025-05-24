ac1b13658a0309a807ebf7d1951fdbd27b5ddf62
rateshsachdeva <rateshsachdeva@gmail.com>
2025-05-24 17:56:59 +0530

    Update document-preview.tsx

--- components/document-preview.tsx
+++ components/document-preview.tsx
@@ -250,10 +250,11 @@ const DocumentContent = ({ document }: { document: ExtendedDocument }) => {
   const containerClassName = cn(
     'h-[257px] overflow-y-scroll border rounded-b-2xl dark:bg-muted border-t-0 dark:border-zinc-700',
     {
-      'p-4 sm:px-14 sm:py-16': document && 'kind' in document && document?.kind === 'text',
-      'p-0': document && 'kind' in document && document?.kind === 'code',
+      'p-4 sm:px-14 sm:py-16': document && 'kind' in document && document.kind === 'text',
+      'p-0': document && 'kind' in document && document.kind === 'code',
     },
   );
+
 
 
   const commonProps = {
